ec9be01ddf3638baab969667a873d35832b323c5
Zhihao <lizhihao_sh@163.com>
2023-12-13 21:31:14 -0800

    update app

--- app.py
+++ app.py
@@ -2,26 +2,46 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-dataset_path = "./datasets/repository_data.csv"
 dataset_url = "https://raw.githubusercontent.com/LeoLi1223/Beacon/main/datasets/github_dataset.csv"
-
 
 @st.cache_data
 def load_dataset(filepath):
     df = pd.read_csv(filepath)
+    df.drop_duplicates(inplace=True)
     return df
 
+# repo of {language} sorted by {stars/forks/issues/pull/contributors} in ascending/descending
+def search_by_lang(df, lang, sorted_key, ascending=True):
+    result = df.sort_values(sorted_key, ascending=ascending)
+    result = result[['repositories', sorted_key, 'language']]
+    result = result[result['language']==lang]
+    if result.shape[0] < 10:
+        return result
+    else:
+        return result.head(10)
 
 df = load_dataset(dataset_url)
 
-langs = st.selectbox(
+lang = st.selectbox(
     "language of interest",
     df['language'].unique()
 )
 
-st.write(langs)
+sort_by = st.selectbox(
+    "sorted by",
+    df.select_dtypes(include='int').columns
+)
 
-# 1. distributions of # stars, # forks, # language used
-# 2. proportion of each language used
-# 3. contributors
-# 4. fetch top repo by # stars, # forks
+is_ascending = st.checkbox(
+    'ascending'
+)
+
+result_df = search_by_lang(df, lang, sort_by, is_ascending)
+st.dataframe(data=result_df)
+
+# search system
+# search option: 
+#   - keyword (text input)
+#   - language (selectbox)
+# sort option: stars, forks, issues, pull_requests, contributors (selectbox)
+# if sorting, enable ascending checkbox
