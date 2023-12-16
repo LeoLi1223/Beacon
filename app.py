9216a4357e8a0e8b4eb3a2c2481325aabb3a9bf0
Zhihao <lizhihao_sh@163.com>
2023-12-16 01:47:12 -0800

    import plt

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import streamlit as st
 
 dataset_url = "https://raw.githubusercontent.com/LeoLi1223/Beacon/main/datasets/github_dataset.csv"
